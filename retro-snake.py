24f34e27b6adc6166e1d418e31e5a2eca0fb03e2
victor-stanciu <victorstanciu.st@gmail.com>
2026-01-15 18:00:06 +0200

    - Add Food class and image. Add rendering of Food object

--- retro-snake.py
+++ retro-snake.py
@@ -1,5 +1,6 @@
-import pygame
+import pygame, sys
 from pygame.math import Vector2
+from random import randint
 
 pygame.init()
 
@@ -8,27 +9,46 @@ DARK_GREEN = (43, 51, 24)
 
 cell_size = 30
 number_of_cells = 25
-
+count = 1
 class Food:
     def __init__(self):
         self.position = Vector2(5,6)
 
-    
+    def draw(self):
+        food_rect = pygame.Rect(
+            self.position.x * cell_size,
+            self.position.y * cell_size,
+            cell_size,
+            cell_size
+        )
+        pygame.draw.rect(screen, DARK_GREEN, food_rect)
+        screen.blit(food_surface, food_rect)
 
-screen = pygame.display.set_mode((750, 750))
+
+screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
+
 
 pygame.display.set_caption("Retro Snake")
 
 clock = pygame.time.Clock()
 
+food = Food()
+food_surface = pygame.image.load("graphics/food.png")
+# food_surface = pygame.transform.scale(food_surface, (30, 30))
+
 while True:
+    pressed_keys = pygame.key.get_pressed()
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    # Drawing
     screen.fill(GREEN)
+    food.draw()
 
     pygame.display.update()
     clock.tick(60)
-    
+
